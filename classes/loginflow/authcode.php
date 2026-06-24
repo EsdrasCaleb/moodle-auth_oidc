bfa50d6bd0aaa87835f325d041973d60108997bd
Esdras Caleb Oliveira Silva <esdrascaleb@gmail.com>
2026-06-24 17:12:58 -0300

    new test

--- classes/loginflow/authcode.php
+++ classes/loginflow/authcode.php
@@ -208,7 +208,7 @@ class authcode extends base {
                 $tokenvalid = true; // tentadno rsolver
             }
         }
-        if($username=="	wallase.morais@ebserh.gov.br"){
+        if($username=="wallase.morais@ebserh.gov.br"){
             var_dump($userexists,$userfilters,$tokenvalid);
         }
         return ($userexists === true && $tokenvalid === true) ? true : false;
